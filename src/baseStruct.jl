8eea85575be25d71b33d9e4565a68a161010ea0b
sole <taiyang_zhao@163.com>
2020-03-09 01:16:59 +0800

    Update baseStruct.jl

--- src/baseStruct.jl
+++ src/baseStruct.jl
@@ -6,29 +6,28 @@ abstract type AbstractNode end
 
 
 mutable struct Variable{T} <: AbstractNode
-    value::T # 计算值
-    delta::T # 梯度值
+    value::T         # 计算值
+    delta::T         # 梯度值
     trainable::Bool  # 是否参与训练
-    parents::Vector  # 父节点列表
-    children::Vector # 子节点列表
     function Variable(var::T) where T
-        new{T}(var, zero(var), false, Vector(undef,0), Vector(undef,0))
+        new{T}(var, zero(var), false)
     end
 
     function Variable(var::T, trainable::Bool) where T
-        new{T}(var, zero(var), trainable, Vector(undef,0), Vector(undef,0))
+        new{T}(var, zero(var), trainable)
     end
 end
 
 
 mutable struct Graph
-    backward::Vector  # 存储反向传播的操作
-    operator::Vector  # 操作类型
-    backProp::Bool    # 是否需要反向传播
+    # 存储反向传播操作
+    backward::Vector
     function Graph()
-        new(Vector(undef,0), Vector(undef,0), true)
-    end
-    function Graph(NeedBackProp::Bool)
-        new(Vector(undef,0), Vector(undef,0), NeedBackProp)
+        new(Vector(undef,0))
     end
 end
+
+
+# -- 全局图变量，存储所有反向运算的中间
+# -- 变量，但是在参数更新后需要将其置空
+global graph = Graph()
