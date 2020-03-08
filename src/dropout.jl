0149fd0c14b1c556368bae906b940b03890ba624
sole <taiyang_zhao@163.com>
2020-03-09 01:16:52 +0800

    Create dropout.jl

---
+++ src/dropout.jl
@@ -0,0 +1,29 @@
+mutable struct dropout <: Block
+    p # dropout probibility
+    dropout(   ) = new(0.1)
+    dropout(pro) = new(pro)
+end
+
+
+function forward(d::dropout, var::Variable)
+    # 对网络激活节点进行灭活
+    # 属于in-place操作,但是输入输出共享节点值引用
+    row, col = size(var.value)
+    RandMask = (rand(row, col) .< (1 - d.p)) ./ (1 - d.p)
+    var.value .*= RandMask
+    out = Variable(var.value, var.trainable)
+    if var.trainable
+        function dropoutBackward()
+            var.delta += RandMask .* out.delta
+        end
+        push!(var.children, out)
+        push!(out.parents,  var)
+        push!(graph.backward, dropoutBackward)
+    end
+    return out, nothing
+end
+
+
+function predicate(d::dropout, input)
+    return input
+end
