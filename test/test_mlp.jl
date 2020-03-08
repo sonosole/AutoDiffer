9ec4bc47ce3bb3b0a497b13be73cea0458fdbb4c
sole <taiyang_zhao@163.com>
2020-03-09 01:16:36 +0800

    Update test_mlp.jl

--- test/test_mlp.jl
+++ test/test_mlp.jl
@@ -1,5 +1,5 @@
 include("../src/mlp.jl")
-
+using Gadfly
 # first class data and lable
 x1 = 3*randn(2,2000);
 label1 = zeros(2,2000);
@@ -17,48 +17,37 @@ for i = 1:length(t)
     x2[2,i] = r[1].*sin(t[i]);
 end
 
-using Gadfly
-# p1 = plot(x=x1[1,:],y=x1[2,:]);
-# p2 = plot(x=x2[1,:],y=x2[2,:]);
-# hstack(p1, p2)
 
 input = hcat(x1,x2)
 label = hcat(label1,label2)
-# plot(x=input[1,:],y=input[2,:])
 
-topology = [2, 32, 32, 2, 2]
-operator = [relu, SIN, sigmoid, softmax]
+
+topology = [2, 32,16,8, 2]
+operator = [leakyrelu, leakyrelu, leakyrelu, softmax]
 mlpmodel = MLP(topology, operator)
 
-mlpmodel.layernum = 5
-epoch = 1000
-lr =  0.0001
+epoch = 1500
+lrate = 1e-5
 lossval = zeros(epoch,1)
 tic = time()
 for i=1:epoch
-    global LOSS
-    g = Graph(true)
-    o, w = forward(g, mlpmodel, input)
-    loss = crossEntropy(g, o, Variable(label))
-    LOSS = cost(g, loss)
-    Backward(g)
-    for i = 1:length(w)
-        update(w[i], lr)
-    end
-    lossval[i] = LOSS.value
+    outs = forward(mlpmodel, Variable(input))
+    COST = binaryCrossEntropyLoss(outs, Variable(label))
+    zerograd(mlpmodel)
+    backward()
+    update(paramsof(mlpmodel), lrate)
+    lossval[i] = COST.value
 end
 toc = time()
-println(" time: ", toc-tic," sec")
-println(" loss: ", LOSS.value)
-p0 = plot(y=lossval, Geom.line);
+println("\n time: ", toc-tic," sec")
+println(" loss: ", lossval[end])
 
 # a predicting example
-mlpmodel.layernum -= 1
-out1 = predicate(mlpmodel, input[:,1:2000] .+ 1e-3)
-out2 = predicate(mlpmodel, input[:,2001:end] .+ 1e-3)
+out1 = predicate(mlpmodel, x1 .+ 1e-4)
+out2 = predicate(mlpmodel, x2 .+ 1e-4)
 
-p1 = plot(x=out1.value[1,:],y=out1.value[2,:],Coord.cartesian(xmin=0, xmax=1, ymin=0, ymax=1),Geom.histogram2d);
-p2 = plot(x=out2.value[1,:],y=out2.value[2,:],Coord.cartesian(xmin=0, xmax=1, ymin=0, ymax=1),Geom.histogram2d);
-h1 = hstack(p1,p2);
-vstack(h1,p0)
-plot(x=out2.value[1,:],y=out2.value[2,:],Geom.histogram2d)
+p0 = plot(y=log.(lossval .+ 1e-100), Geom.line);
+p1 = plot(
+layer(x=out1[1,:],y=out1[2,:],Theme(default_color=colorant"blue")),
+layer(x=out2[1,:],y=out2[2,:],Theme(default_color=colorant"red")))
+vstack(p0,p1)
