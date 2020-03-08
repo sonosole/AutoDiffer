708e1c4381315a1ce401443eb10172cc369acdd1
sole <taiyang_zhao@163.com>
2020-03-09 01:17:08 +0800

    Update checkad.jl

--- checkGrad/checkad.jl
+++ checkGrad/checkad.jl
@@ -4,30 +4,28 @@ include("../src/AutoDiffer.jl")
 m = [ 1. -2.;
      -3.  4.;
       5. -6.]
-g = Graph()
-x = Variable(m)
-y = relu(g, x)
+x = Variable(m,true)
+y = relu(x)
 y.delta = [-0.1 0.2;
             0.3 0.4;
             0.5 0.6]
 
-g.backward[1]()
+backward()
 print(x.delta)
-[-0.1 0.0;
-  0.0 0.4;
-  0.5 0.0]
+# [-0.1 0.0;
+#   0.0 0.4;
+#   0.5 0.0]
 
 # 测试 leakyrelu
 m = [ 1. -2.;
      -3.  4.;
       5. -6.]
-g = Graph()
-x = Variable(m)
-y = leakyrelu(g, x)
+x = Variable(m,true)
+y = leakyrelu(x)
 y.delta = [-0.1 0.2;
             0.3 0.4;
             0.5 0.6]
-g.backward[1]()
+backward()
 print(x.delta)
 
 
@@ -36,15 +34,14 @@ m = [ 1. 2.;
       1. 2.;
       1. 2.;
       1. 2.;]
-g = Graph()
-x = Variable(m)
-y = scalarAddMat(g, x, 0.1)
-z = softmax(g, y)
+x = Variable(m,true)
+y = x * 0.1
+z = softmax(y)
 z.delta =-[1.1 1.9;
            1.2 1.8;
            1.3 1.7;
            1.4 1.6]
-Backward(g)
+backward()
 print(x.delta)
 
 
@@ -63,7 +60,6 @@ param[2] = zeros(128,1)
 param[3] = randn(4,128)./10
 param[4] = zeros(4,1)
 
-g = Graph(true)
 l = Variable(label) # label
 x = Variable(input) # input
 
@@ -71,33 +67,25 @@ w1 = Variable(param[1],true) # weight1
 b1 = Variable(param[2],true) # bias1
 w2 = Variable(param[3],true) # weight2
 b2 = Variable(param[4],true) # bias2
-x1 = relu(g, matAddVec(g, matMul(g,w1,x), b1))
-x2 = softmax(g, matAddVec(g, matMul(g,w2,x1), b2))
-loss = crossEntropy(g, x2, l)
-LOSS1 = cost(g, loss)
-Backward(g)
+x1 = relu(matAddVec(w1*x, b1))
+x2 = softmax(matAddVec(w2*x1, b2))
+loss = crossEntropy(x2^10, l)
+LOSS1 = cost(loss)
+backward()
 backwardGrad = w1.delta[1,1]
 
 
 delta = 1e-6
 param[1][1,1] += delta
 
-g = Graph(true)
-l = Variable(label) # label
-x = Variable(input) # input
+x1 = relu(matAddVec(w1*x, b1))
+x2 = softmax(matAddVec(w2*x1, b2))
+loss = crossEntropy(x2^10, l)
+LOSS2 = cost(loss)
+backward()
 
-w1 = Variable(param[1],true) # weight1
-b1 = Variable(param[2],true) # bias1
-w2 = Variable(param[3],true) # weight2
-b2 = Variable(param[4],true) # bias2
-x1 = relu(g, matAddVec(g, matMul(g,w1,x), b1))
-x2 = softmax(g, matAddVec(g, matMul(g,w2,x1), b2))
-loss = crossEntropy(g, x2, l)
-LOSS2 = cost(g, loss)
-Backward(g)
-
-forwardGrad = ( LOSS2.value[1]-LOSS1.value )/delta
+forwardGrad = ( LOSS2.value - LOSS1.value )/delta
 println("\n---------------------")
-println(" forwardGrad:", forwardGrad)
+println(" forwardGrad:",  forwardGrad)
 println("backwardGrad:", backwardGrad)
 println("---------------------")
