0dcaddd896a7b5bc857c5aa61ce39023fe5e7732
sole <taiyang_zhao@163.com>
2020-03-30 00:28:39 +0800

    Update check_grad_mlp.jl

--- checkGrad/check_grad_mlp.jl
+++ checkGrad/check_grad_mlp.jl
@@ -2,27 +2,27 @@ include("../src/mlp.jl")
 
 # [1] prepare input data and its label
 x = randn(256, 62)
-l = rand(10, 62)
+l = rand(64, 62)
 l = l ./ sum(l,dims=1)
-m = MLP([256, 128,32,16,8, 10], [sin,relu,softplus,sigmoid,softmax])
+m = MLP([256, 128,128,128,128,128, 64], [relu,relu,relu,relu,relu,softmax])
 
 # [2] forward and backward propagation
-o = forward(m, Variable(x))
-loss = crossEntropy(o, Variable(l))
+outs = forward(m, Variable(x))
+loss = crossEntropy(outs, Variable(l))
 LOSS1 = cost(loss)
 backward()
-GRAD = m.parameter[1].w.delta[1,1]
+GRAD = m.parameters[1].w.delta[1,1]
 println("\n---------------------------------")
 println("反向传播梯度: ", GRAD)
 
 
 # [3] forward and backward propagation with a samll change of a weight
 DELTA = 1e-7
-m.parameter[1].w.value[1,1] += DELTA
+m.parameters[1].w.value[1,1] += DELTA
 
 time1 = time()
-o = forward(m, Variable(x))
-loss = crossEntropy(o, Variable(l))
+outs = forward(m, Variable(x))
+loss = crossEntropy(outs, Variable(l))
 LOSS2 = cost(loss)
 time2 = time()
 backward()
@@ -31,7 +31,7 @@ time3 = time()
 # [4] check if the auto-grad is true or not
 dLdW = (LOSS2.value - LOSS1.value)/DELTA
 err  = abs((dLdW-GRAD)/GRAD)
-err  = err < 1/100000 ? 0.0 : err
+err  = err < 1e-5 ? 0.0 : err
 println("数值计算梯度: ", dLdW)
 println("相对梯度误差：", err," %")
 
