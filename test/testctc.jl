36729d5ff9d46bbd03641667ff2f7506205ff0ad
sole <taiyang_zhao@163.com>
2020-03-30 00:31:40 +0800

    Create testctc.jl

---
+++ test/testctc.jl
@@ -0,0 +1,16 @@
+include("../src/baseStruct.jl")
+include("../src/baseOperator.jl")
+include("../src/ctc.jl")
+
+
+function testctcfn()
+    S = 5
+    T = 10000
+    x = 0.000005*rand(S,T)
+    P = Variable(x,true)
+
+    r,loglikely = DNN_CTC_Without_Softmax(P, [2 3 4 5 2 3 4 5])
+    println("loglikely = ",loglikely)
+    backward()
+    return nothing
+end
