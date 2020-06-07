59f35e642d8446e9f80bc7ec044d91886527863c
sole <taiyang_zhao@163.com>
2020-06-07 23:43:30 +0800

    Update AutoDiffer.jl

--- src/AutoDiffer.jl
+++ src/AutoDiffer.jl
@@ -3,4 +3,14 @@
 include("./baseStruct.jl")
 include("./baseOperator.jl")
 
+include("./dropout.jl")
+include("./linear.jl")
+include("./dense.jl")
+include("./irnn.jl")
+include("./P1Relu.jl")
+include("./maxout.jl")
+include("./residual.jl")
+include("./chain.jl")
+include("./optimizer.jl")
+include("./ctc.jl")
 # end  # moduleautodiffer
