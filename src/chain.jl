8796d4db7defb317fcc929b9907d3cea3b450bab
sole <taiyang_zhao@163.com>
2020-06-07 23:43:40 +0800

    Update chain.jl

--- src/chain.jl
+++ src/chain.jl
@@ -1,26 +1,83 @@
 mutable struct Chain
-    blocks::Array{Block,1}
-    function Chain(sequence::Array{Block,1})
-        len = length(sequence)
-        blocks = Vector(undef,len)
-        for i = 1:len
+    blocknum::Int
+    blocks::Vector
+    function Chain(sequence::Vector)
+        blocknum = length(sequence)
+        blocks = Vector(undef,blocknum)
+        for i = 1:blocknum
             blocks[i] = sequence[i]
         end
+        new(blocknum, blocks)
+    end
+    function Chain(sequence...)
+        blocknum = length(sequence)
+        blocks = Vector(undef,blocknum)
+        for i = 1:blocknum
+            blocks[i] = sequence[i]
+        end
+        new(blocknum, blocks)
     end
 end
 
 
-function forward(seq::Chain, input::Variable)
-    var = Vector(undef, 0)
-    x,w = forward(seq.blocks[1], input)
-    push!(var, w)
-
-    for block in seq.blocks[2:end]
-        x, w = forward(block, x)
-        push!(var, w)
+function paramsof(c::Chain)
+    params = Vector{Variable}(undef,0)
+    for i = 1:c.blocknum
+        p = paramsof(c.blocks[i])
+        if p != nothing
+            append!(params, p)
+        end
     end
-    return  x, var
+    return params
 end
 
 
-#
+function nparamsof(c::Chain)
+    k = 0
+    for i = 1:c.blocknum
+        k += nparamsof(c.blocks[i])
+    end
+    return k
+end
+
+
+function resethidden(c::Chain)
+    for i = 1:c.blocknum
+        if c.blocks[i] isa irnn
+            resethidden(c.blocks[i])
+        end
+        if c.blocks[i] isa IRNN
+            resethidden(c.blocks[i])
+        end
+        # if c.blocks[i] isa lstm
+        #     resethidden(c.blocks[i])
+        # end
+        # if c.blocks[i] isa gru
+        #     resethidden(c.blocks[i])
+        # end
+        # if c.blocks[i] isa LSTM
+        #     resethidden(c.blocks[i])
+        # end
+        # if c.blocks[i] isa GRU
+        #     resethidden(c.blocks[i])
+        # end
+    end
+end
+
+
+function forward(c::Chain, input::Variable)
+    x = forward(c.blocks[1], input)
+    for i = 2:c.blocknum
+        x = forward(c.blocks[i], x)
+    end
+    return x
+end
+
+
+function predict(c::Chain, input)
+    x = predict(c.blocks[1], input)
+    for i = 2:c.blocknum
+        x = predict(c.blocks[i], x)
+    end
+    return x
+end
