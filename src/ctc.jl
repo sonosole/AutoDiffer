d5e8f7c813d80a15f61450a346ead5a28e960202
sole <taiyang_zhao@163.com>
2020-06-07 23:44:19 +0800

    Update ctc.jl

--- src/ctc.jl
+++ src/ctc.jl
@@ -1,9 +1,12 @@
+NINF = -floatmax(Float32)
+
+
 function LogSum2Exp(a::Real, b::Real)
-	if a <= -1.797e308
-        a = -1.797e308
+	if a <= NINF
+        a = NINF
     end
-	if b <= -1.797e308
-        b = -1.797e308
+	if b <= NINF
+        b = NINF
     end
 	return (max(a,b) + log(1.0 + exp(-abs(a-b))));
 end
@@ -15,7 +18,7 @@ end
 
 
 function LogSumExp(a)
-    tmp = -1.797e308
+    tmp = NINF
     for i = 1:length(a)
         tmp = LogSum2Exp(tmp,a[i])
     end
@@ -26,16 +29,17 @@ end
 function CTC(p::Array{TYPE,2}, seq) where TYPE
     # inputs:
     # p      -- probability of softmax output
-    # seq    -- label seq
+    # seq    -- label seq like [9 2 3 6 5 2 2 3]
+	#           1 is blank,so minimum of it is 2
     # outputs:
-    # r      -- target of softmax output
+    # r      -- target of softmax's output
     # logsum -- log-likelyhood
 
     S,T = size(p)
     L = length(seq)*2 + 1
-    a = fill(-floatmax(), L,T) # alpha
-    b = fill(-floatmax(), L,T) # beta
-    r = fill(-floatmax(), S,T) # gamma
+    a = fill(NINF, L,T) # alpha = p(s[k,t], x[1:t])
+    b = fill(NINF, L,T) # beta  = p(x[t+1:T] | s[k,t])
+    r = fill(NINF, S,T) # gamma = classWiseSum(alpha * beta)
 
     if L>1
         a[1,1] = log(p[    1, 1])
@@ -88,7 +92,7 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
         end
     end
 
-    logsum = - floatmax()
+    logsum = NINF
     for s = 1:L
         logsum = LogSum2Exp(logsum, a[s,1] + b[s,1])
     end
@@ -102,13 +106,30 @@ function CTC(p::Array{TYPE,2}, seq) where TYPE
         end
     end
     r .= exp.(r .- logsum)
-    return r, logsum
+    return r, -logsum
+end
+
+
+function greedysearch(x)
+	# 除了blank作为吸收态外，还有一个单独的状态作为集外词的吸收态
+	# 并且约定将 blank 映射到 1，OOV映射到 2,集内词从3开始映射.
+    dog =  1
+    out = [1]
+    idx = argmax(x,dims=1)
+    for i = 1:length(idx)
+        maxid = idx[i][1]
+        if (maxid != 1) && (maxid != 2) && (maxid != out[dog])
+            push!(out,maxid)
+            dog += 1
+        end
+    end
+    return out[2:end]
 end
 
 
 function testctc()
-    S = 5
-    T = 30000
+    S = 500
+    T = 100
     x = 0.01*rand(S,T)
     P = x
 
@@ -117,47 +138,80 @@ function testctc()
         P[:,t] = P[:,t] ./ sum(P[:,t])
     end
     tic = time()
-    r,loglikely = CTC(P,[2 3 4 5 2 3 4 5])
+    r,loglikely = CTC(P,[3 3 4 5 2 3 4 5])
     toc = time()
-    println("loglikely = ",loglikely)
+    println("loglikely = ",loglikely/T," (timeSteps averaged)")
     println("ctc_time  = ",(toc-tic)*1000," ms")
-    Gadfly.plot(
-    layer(y=r[1,:],Geom.line,Theme(default_color=colorant"red")),
-    layer(y=r[2,:],Geom.line,Theme(default_color=colorant"yellow")),
-    layer(y=r[3,:],Geom.line,Theme(default_color=colorant"blue")),
-    layer(y=r[4,:],Geom.line,Theme(default_color=colorant"green")),
-    layer(y=r[5,:],Geom.line,Theme(default_color=colorant"orange"))
-    )
-
-
+	# using Gadfly
+    # Gadfly.plot(
+    # layer(y=r[1,:],Geom.line,Theme(default_color=colorant"red")),
+    # layer(y=r[2,:],Geom.line,Theme(default_color=colorant"yellow")),
+    # layer(y=r[3,:],Geom.line,Theme(default_color=colorant"blue")),
+    # layer(y=r[4,:],Geom.line,Theme(default_color=colorant"green")),
+    # layer(y=r[5,:],Geom.line,Theme(default_color=colorant"orange"))
+    # )
 end
 
 
-@time testctc()
-
-function DNN_CTC_Without_Softmax(var::Variable, seq)
-    row, col = size(var.value)
-    fac = length(seq) / col
+function DNN_CTC_With_Softmax(var::Variable, seq)
+    row, col = size(var)
+    fac = (length(seq)+1) / col
     out = Variable(zeros(row, col), var.trainable)
-
-    Xmax = maximum(var.value, dims=1)
-    out.value = exp.(var.value .- Xmax)
-    out.value ./= sum(out.value, dims=1)
+	out.value = softmax(var.value)
     r, loglikely = CTC(out.value, seq)
 
     if var.trainable
-        function DNN_CTC_Without_Softmax_Backward()
+        function DNN_CTC_With_Softmax_Backward()
             var.delta += (out.value - r) .* fac
         end
-        push!(graph.backward, DNN_CTC_Without_Softmax_Backward)
+        push!(graph.backward, DNN_CTC_With_Softmax_Backward)
     end
     return out, loglikely
 end
 
 
-function DNN_CTC_Without_Softmax(x::Array)
-    xmax = maximum(x, dims=1)
-    prob = exp.(x .- xmax)
-    psum = sum(prob, dims=1)
-    return (prob ./ psum)
+function indexbounds(sizeArray)
+	# assert sizeArray has no 0 element
+    acc = 0
+    num = length(sizeArray)
+    s = ones(Int,num,1)
+    e = ones(Int,num,1)
+    for i = 1:num
+        s[i] += acc
+        e[i] = s[i] + sizeArray[i] - 1
+        acc += sizeArray[i]
+    end
+    return (s,e)
+end
+
+
+function DNN_Batch_CTC_With_Softmax(var::Variable, seq, inputSizeArray, labelSizeArray)
+    row, col = size(var)
+    out = Variable(zeros(row, col), var.trainable)
+
+	Loglikely = 0.0
+	batchsize = length(inputSizeArray)
+	out.value = softmax(var.value)
+	gamma     = zero(out.value)
+	sidI,eidI = indexbounds(inputSizeArray)
+	sidL,eidL = indexbounds(labelSizeArray)
+
+	for i = 1:batchsize
+		IDI = sidI[i]:eidI[i]
+		IDL = sidL[i]:eidL[i]
+		len = length(IDI)
+		fac = (length(IDL)+1) / len
+		gamma[:,IDI], loglikely = CTC(out.value[:,IDI], seq[IDL])
+		gamma[:,IDI] .*= fac
+		out.value[:,IDI] .*= fac
+		Loglikely += loglikely
+	end
+
+    if var.trainable
+        function DNN_Batch_CTC_With_Softmax_Backward()
+            var.delta += out.value - gamma
+        end
+        push!(graph.backward, DNN_Batch_CTC_With_Softmax_Backward)
+    end
+    return out, Loglikely/batchsize
 end
